1ac32109ea9fa5e7c71c0af6884a50280bd80ce9
SHRUTI BANERJEE <86586883+ShrutiBanerjee03@users.noreply.github.com>
2021-08-04 12:22:20 +0530

    Bitwise Operators in java

---
+++ Bitwise_Operator.java
@@ -0,0 +1,19 @@
+
+public class Bitwise_Operator {
+
+	public static void main(String[] args) {
+		int a=12,b=25,c,d,e;
+		c=a|b;
+		System.out.println(c);
+		d=a&b;
+		System.out.println(d);
+		e=a^b;
+		System.out.println(e);
+		int f=~b;
+		System.out.println(f);
+		int g=~a;
+		System.out.println(g);
+
+	}
+
+}
