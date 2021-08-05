f2a7b0c326b37acf6be0fe27f647ea63c1cc4ed8
SHRUTI BANERJEE <86586883+ShrutiBanerjee03@users.noreply.github.com>
2021-08-05 15:42:41 +0530

    Shift operators

---
+++ Shift_Operators.java
@@ -0,0 +1,15 @@
+
+public class Shift_Operators {
+
+	public static void main(String[] args) {
+		int a=8,b=-8,c=2;
+		System.out.println(a>>2);
+		System.out.println(b>>2);
+		System.out.println(c<<2);
+
+		
+		
+
+	}
+
+}
